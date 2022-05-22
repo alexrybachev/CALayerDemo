5532c4d37e25ace214dab17ac687452b7f8864e6
Alex Rybachev <rybachev.as@gmail.com>
2022-05-22 12:23:56 +0400

    add CAShapeLayer

--- CALayerDemo/ViewController.swift
+++ CALayerDemo/ViewController.swift
@@ -9,6 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var shapeLayer: CAShapeLayer! {
+        didSet {
+            shapeLayer.lineWidth = 20
+            shapeLayer.lineCap = .round
+            shapeLayer.fillColor = nil
+            shapeLayer.strokeEnd = 1
+            let color = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1).cgColor
+            shapeLayer.strokeColor = color
+        }
+    }
+    
+    var overShapeLayer: CAShapeLayer! {
+        didSet {
+            overShapeLayer.lineWidth = 20
+            overShapeLayer.lineCap = .round
+            overShapeLayer.fillColor = nil
+            overShapeLayer.strokeStart = 0
+            overShapeLayer.strokeEnd = 0
+            let color = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+            overShapeLayer.strokeColor = color
+        }
+    }
+    
     var gradientLayer: CAGradientLayer! {
         didSet {
             gradientLayer.startPoint = CGPoint(x: 0, y: 0)
@@ -41,6 +64,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         gradientLayer = CAGradientLayer()
         view.layer.insertSublayer(gradientLayer, at: 0)
+        
+        shapeLayer = CAShapeLayer()
+        view.layer.addSublayer(shapeLayer)
+        
+        overShapeLayer = CAShapeLayer()
+        view.layer.addSublayer(overShapeLayer)
     }
     
     override func viewDidLayoutSubviews() {
@@ -49,7 +78,27 @@ class ViewController: UIViewController {
             x: 0,
             y: 0,
             width: view.bounds.size.width,
-            height: view.bounds.size.height)
+            height: view.bounds.size.height
+        )
+        
+        configShapeLayer(shapeLayer)
+        configShapeLayer(overShapeLayer)
     }
+    
+    private func configShapeLayer(_ shapeLayer: CAShapeLayer) {
+        shapeLayer.frame = view.bounds
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: view.frame.width / 2 - 100, y: view.frame.height / 2))
+        path.addLine(to: CGPoint(x: view.frame.width / 2 + 100, y: view.frame.height / 2))
+        shapeLayer.path = path.cgPath
+    }
+    
+    @IBAction func buttunTapped() {
+        overShapeLayer.strokeEnd += 0.2
+        if overShapeLayer.strokeEnd == 1 {
+            performSegue(withIdentifier: "showSecondVC", sender: nil)
+        }
+    }
+    
 }
 
